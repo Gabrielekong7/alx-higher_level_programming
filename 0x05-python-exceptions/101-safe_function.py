cef544a0ef85f48e81f6b749a43795c010739566
Gabrielekong7 <gabrielekong533@gmail.com>
2023-10-23 14:35:22 -0700

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,14 @@
+101-safe_function.py
+
+
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        num = fct(*args)
+        return num
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
