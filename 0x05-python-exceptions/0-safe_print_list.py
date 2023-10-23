75d3b08f16ad2576cc7f6161aa6fbcf8392405d4
Gabrielekong7 <gabrielekong533@gmail.com>
2023-10-23 08:13:53 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    # function that prints x elements of a list.
+    printed_elements = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end=' ')
+            printed_elements += 1
+    except IndexError:
+        pass
+
+    print()
+    return printed_elements
