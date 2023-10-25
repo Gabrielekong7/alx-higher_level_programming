8f133314b9a191a341ffd60c18620cb7fd322ec0
Gabrielekong7 <gabrielekong533@gmail.com>
2023-10-25 15:22:50 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Square module."""
+
+
+class square:
+    """ Define a square."""
+
+    def __init__(self, self=0):
+        """Constructor.
+
+        Args:
+            size: Lenght of the square.
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less then 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        """Area of the Square.
+
+        Returns:
+            the size squred.
+        """
+        return self.__size ** 2
