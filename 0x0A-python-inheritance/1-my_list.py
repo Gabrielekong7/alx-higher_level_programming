29da0711e064c8fa6c83acd2897731c56116b4a2
Gabrielekong7 <gabrielekong533@gmail.com>
2023-11-06 11:27:33 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Define an inherited list Mylist."""
+
+def print_sorted(self):
+    """Implements sorted printing for the built-in list class."""
+    def print_sorted(self):
+        """prints the list but sorted ascending sort"""
